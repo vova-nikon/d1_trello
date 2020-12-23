dd34f77b97b96e9e496f759118a31b0a3f42f19d
Vladimir Nikonov <60705589+vova-nikon@users.noreply.github.com>
2020-12-24 00:43:13 +0800

    Update trello_upload.py

--- trello_upload.py
+++ trello_upload.py
@@ -25,76 +25,68 @@ def read():
         for task in task_data:
             print('\t' + task['name'])
 
+
 def create(task_name, column_name):
-    tasks = []
     for column in column_data:
         if column['name'] == column_name:
-            task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
-            for task in task_data:
-                tasks.append(task['name'])
-            if task_name in tasks:
-                print('The card "{}" already exists in this List. Please choose a different name or update the name of the other card.'.format(task_name))
-                break
-            else:
-                requests.post(base_url.format('cards'), data={'name': task_name, 'idList': column['id'], **auth_params})
-                print('Success!')
-                break
+            requests.post(base_url.format('cards'), data={'name': task_name, 'idList': column['id'], **auth_params})
+            print('Success!')
+            break
+
 
 def create_column(column_name):
     columns = []
     for column in column_data:
         columns.append(column['name'])
     if column_name in columns:
-        print('The List "{}" already exists. Please choose a different name or update the name of the other card by using update_card(old_name, new_name, column_name).'.format(column_name))
+        print('The List "{}" already exists. Please choose a different name.'.format(column_name))
     else:
         requests.post(base_url.format('boards') + '/' + board_id + '/lists', data={'name': column_name, **auth_params})
         print('Success!')
 
+
 def move(task_name, column_name):
-    tasks = []
     task_id = None
+    tasks = []
 
     for column in column_data:
         task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
 
         for task in task_data:
             if task['name'] == task_name:
-                task_id = task['id']
-                break
-        if task_id:
-            break
+                task_obj = {'num': len(tasks) + 1, 'name': task['name'], 'id': task['id'], 'column': column['name']}
+                tasks.append(task_obj)
 
-    for column in column_data:
-        if column['name'] == column_name:
-            task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
-            for task in task_data:
-                tasks.append(task['name'])
-            if task_name in tasks:
-                print('The card "{}" already exists in the list "{}". Please choose a different name or update the name of the other card by using update_card(old_name, new_name, column_name).'.format(task_name, column_name))
-                break
-            else:
-                requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'], **auth_params})
-                print('Success!')
-                break
-
-
-def update_card(old_name, new_name, column_name):
-    tasks = []
-    task_id = None
-    for column in column_data:
-        if column['name'] == column_name:
-            task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
-            for task in task_data:
-                tasks.append(task['name'])
-            if new_name in tasks:
-                print('The card "{}" already exists in the list "{}". Please choose a different name or update the name of the other card by using update_card(old_name, new_name, column_name).'.format(new_name, column_name))
-                break
-            else:
-                if task['name'] == old_name:
+    if len(tasks) > 1:
+        print('There are {} cards with the name "{}".'.format(len(tasks), task_name))
+        for task in tasks:
+            print('{}. {} ({}) -- {}'.format(task['num'], task['name'], task['column'], task['id']))
+        num_to_move = int(input('Which of them would you like to move? Please enter the number: '))
+        if num_to_move > len(tasks):
+            print('Incorrect input')
+        else:
+            for task in tasks:
+                if task['num'] == num_to_move:
                     task_id = task['id']
-                    requests.put(base_url.format('cards') + '/' + task_id, data={'name': new_name, **auth_params})
-                    print('Success!')
                     break
+            for column in column_data:    
+                if column['name'] == column_name:    
+ 
+                    requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'], **auth_params})    
+                    break 
+
+
+    elif len(tasks) == 1:
+        task_id = tasks[0]['id']
+
+        for column in column_data:
+            if column['name'] == column_name:    
+                requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'], **auth_params})    
+     
+
+    else:
+        print('No card "{}" found.'.format(task_name))
+
 
 
 
@@ -108,7 +100,3 @@ if __name__ == "__main__":
         create(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == 'move':
         move(sys.argv[2], sys.argv[3])
-    elif sys.argv[1] == 'update_card':
-        update_card(sys.argv[2], sys.argv[3], sys.argv[4])
-
-
